198da1b8651e02112de8e916b135a739cb097c4a
cesare rocchetti <cesare@cr-consult.eu>
2016-02-27 10:56:17 +0100

    Inizio l'aggancio del WS

--- MailTicketAzureMvc/Controllers/HomeController.cs
+++ MailTicketAzureMvc/Controllers/HomeController.cs
@@ -8,21 +8,29 @@ namespace MailTicketAzureMvc.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
+            if (User.IsInRole("Admin"))
+            {
+                ViewBag.Title = "Home amministratore";
+                ViewBag.Message = "Gestisci i punti vendita";
+            }
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Title = "Istruzioni per l'uso";
+            ViewBag.Message = "A cosa serve e come si usa il portale";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Title = "Contattaci";
+            ViewBag.Message = "Se hai domande o non riesci a collegarti contattaci pure";
 
             return View();
         }
