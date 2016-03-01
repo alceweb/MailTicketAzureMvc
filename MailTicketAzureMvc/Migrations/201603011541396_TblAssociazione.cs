d63114f476769893941f68025b6acebf22daaac8
cesare rocchetti <cesare@cr-consult.eu>
2016-03-01 16:42:16 +0100

    TblAssociazione

---
+++ MailTicketAzureMvc/Migrations/201603011541396_TblAssociazione.cs
@@ -0,0 +1,16 @@
+namespace MailTicketAzureMvc.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TblAssociazione : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ MailTicketAzureMvc/Models/Associazione.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MailTicketAzureMvc.Models
+{
+    public class Associazione
+    {
+        public string IdUtente { get; set; }
+        public string IdMan { get; set; }
+    }
+}
